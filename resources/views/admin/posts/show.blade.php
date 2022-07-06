d865deeb1619eb34410d794beb17b432e7224175
Pietroarmill <pietroarmillotta38@gmail.com>
2022-07-07 00:49:54 +0200

    add edit and update in PostController

--- resources/views/admin/posts/show.blade.php
+++ resources/views/admin/posts/show.blade.php
@@ -4,4 +4,8 @@
   <h1>{{ $post->title }}</h1>
   <p>Slug: {{ $post->slug }}</p>
   <p>{{ $post->content }}</p>
+  <div>
+    <a class="btn btn-primary "href="{{ route('admin.posts.edit', ['post' => $post->id]) }}">Edit</a>
+    <a class="btn btn-danger" href="">Delete</a>
+  </div>
 @endsection
