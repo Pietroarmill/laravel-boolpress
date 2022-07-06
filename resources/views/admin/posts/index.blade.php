2bd5c5e4ed537323c097f144f39de14c233aa6ab
Pietroarmill <pietroarmillotta38@gmail.com>
2022-07-06 18:41:36 +0200

    add post.show and route to post->id

--- resources/views/admin/posts/index.blade.php
+++ resources/views/admin/posts/index.blade.php
@@ -13,7 +13,7 @@
       <div class="card-body">
         <h5 class="card-title">{{ $post->title }}</h5>
         {{-- <p class="card-text">{{ $post->content }}</p> --}}
-        <a href="#" class="btn btn-primary">Apri post</a>
+        <a href="{{ route('admin.posts.show', ['post' => $post->id ]) }}" class="btn btn-primary">Apri post</a>
       </div>
     </div>
   </div>
